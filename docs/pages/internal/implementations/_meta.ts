0c578ccecdb174d3053fea6b727cfe1b5baa9ecb
Joy <jtas.sarkar@gmail.com>
2026-05-23 07:45:45 +0530

    docs(implementations): add frontend IAM task list

--- docs/pages/internal/implementations/_meta.ts
+++ docs/pages/internal/implementations/_meta.ts
@@ -1,6 +1,7 @@
 export default {
   index: 'Master Implementations',
   tasks: 'Implementation Tasks',
+  'frontend-tasks': 'Frontend Tasks',
   changelog: 'Changelog',
   'tech-stack': 'Tech Stack',
   'project-setup': 'Project Setup',
